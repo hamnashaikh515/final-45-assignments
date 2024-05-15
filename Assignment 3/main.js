e2401ad74f6cba1b665fe76b754a01bbde9c4588
Hamna Shaikh <hamnashaikh515@gmail.com>
2024-05-14 19:43:54 -0700

    first commit

---
+++ Assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let qoute = "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world";
+let author = "Albert Einstein";
+//printng the message 
+console.log(`"${qoute}" - ${author}`);
---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let qoute:string = "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world"
+
+let author:string = "Albert Einstein";
+
+//printng the message 
+
+console.log(`"${qoute}" - ${author}`);
