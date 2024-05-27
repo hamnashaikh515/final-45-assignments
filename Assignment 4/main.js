08361f5ea7096a271427be716e5901321769317f
Hamna Shaikh <160252803+hamnashaikh515@users.noreply.github.com>
2024-05-27 08:33:10 +0500

    first commit

---
+++ Assignment 4/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignment 4
+//store the famous person’s name in a variable called famous_person
+let famous_person = "Albert Einstein";
+//compose your message and store it in a new variable called message
+let quote = "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world";
+let message = `${famous_person} once said, ${quote}`;
+//print your message
+console.log(message);
---
+++ Assignment 4/main.ts
@@ -0,0 +1,17 @@
+//Assignment 4
+
+//store the famous person’s name in a variable called famous_person
+
+let famous_person: string = "Albert Einstein";
+
+//compose your message and store it in a new variable called message
+
+let quote: string =
+  "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world";
+
+  let message= `${famous_person} once said, ${quote}`
+
+  //print your message
+
+  console.log(message);
+  
